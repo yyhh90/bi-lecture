85265c7ded8107c6c3670b92d523a6814b9497d0
yyhh90 <hyyn9010G@gmail.com>
2024-06-09 23:49:49 +0900

    0508

---
+++ my_folder/28691.py
@@ -0,0 +1,12 @@
+name = str(input())
+
+if name == "M":
+    print("MatKor")
+elif name == "W":
+    print("WiCys")
+elif name == "C":
+    print("CyKor")
+elif name == "A":
+    print("AlKor")
+elif name == "$":
+    print("$clear")
