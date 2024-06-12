c2e99fa7b237b09974a4fd983849cefa36c005b0
yyhh90 <hyyn9010G@gmail.com>
2024-06-12 15:37:51 +0900

    0527

---
+++ my_folder/28235.py
@@ -0,0 +1,13 @@
+A = input()
+
+if A == "SONGDO":
+    print("HIGHSCHOOL")
+
+elif A == "CODE":
+    print("MASTER")
+
+elif A == "2023":
+    print("0611")
+
+elif A == "ALGORITHM":
+    print("CONTEST")
